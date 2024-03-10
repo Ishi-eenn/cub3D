1abfe16bce7b868d8d1e1a16fc7242855b6d5570
Ishi <syi378039@gmail.com>
2024-03-10 17:57:18 +0900

    条件式を関数分割

--- srcs/parser/map/check_closed.c
+++ srcs/parser/map/check_closed.c
@@ -6,23 +6,40 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 17:38:40 by tsishika          #+#    #+#             */
-/*   Updated: 2024/03/10 17:49:00 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/03/10 17:56:59 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
+
+bool title3(t_data *data, size_t x, size_t y)
+{
+	return (data->cub->map->map_data[y][x] == '0' && (x == 0 || y == 0 || x == data->cub->map->width - 1 || y == data->cub->map->height - 1 ||
+	data->cub->map->map_data[y - 1][x] == ' ' || data->cub->map->map_data[y + 1][x] == ' ' || data->cub->map->map_data[y][x - 1] == ' ' || data->cub->map->map_data[y][x + 1] == ' '));
+}
 
 void title2(t_data *data, size_t y){
 	size_t	x;
 
 	x = 0;
 	while(x < data->cub->map->width){
-		if (data->cub->map->map_data[y][x] == '0' && (x == 0 || y == 0 || x == data->cub->map->width - 1 || y == data->cub->map->height - 1 ||
-		data->cub->map->map_data[y - 1][x] == ' ' || data->cub->map->map_data[y + 1][x] == ' ' || data->cub->map->map_data[y][x - 1] == ' ' || data->cub->map->map_data[y][x + 1] == ' '))
-		print_error_and_exit("Map is not closed or not valid");
+		if (title3(data, x, y))
+			print_error_and_exit("Map is not closed or not valid");
 		x++;
 	}
 }
+
+// void title2(t_data *data, size_t y){
+// 	size_t	x;
+
+// 	x = 0;
+// 	while(x < data->cub->map->width){
+// 		if (data->cub->map->map_data[y][x] == '0' && (x == 0 || y == 0 || x == data->cub->map->width - 1 || y == data->cub->map->height - 1 ||
+// 		data->cub->map->map_data[y - 1][x] == ' ' || data->cub->map->map_data[y + 1][x] == ' ' || data->cub->map->map_data[y][x - 1] == ' ' || data->cub->map->map_data[y][x + 1] == ' '))
+// 		print_error_and_exit("Map is not closed or not valid");
+// 		x++;
+// 	}
+// }
 
 void title(t_data *data){
 	size_t	y;
