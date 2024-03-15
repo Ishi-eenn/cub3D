0c930b5a19ff43f76b4bd0af927f6cfbacba8886
Ishi <syi378039@gmail.com>
2024-03-15 15:56:48 +0900

    imgの初期化

--- srcs/draw/init_mlx.c
+++ srcs/draw/init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 15:13:32 by tsishika          #+#    #+#             */
-/*   Updated: 2024/03/15 15:52:12 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/03/15 15:56:32 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,18 @@ void	init_texture(t_data *data)
 	xpm_file_to_img(data, &data->mlx->texture[3], data->cub->texture_path->east);
 }
 
+void	init_img(t_data *data)
+{
+	data->mlx->img->img_ptr = mlx_new_image(data->mlx->mlx,
+			WINDOW_WIDTH, WINDOW_HEIGHT);
+	if (!data->mlx->img->img_ptr)
+		print_error_and_exit("mlx_new_image failed\n");
+	data->mlx->img->addr = mlx_get_data_addr(data->mlx->img->img_ptr,
+			&data->mlx->img->bits_per_pixel,
+			&data->mlx->img->size_line,
+			&data->mlx->img->endian);
+}
+
 void	init_mlx(t_data *data)
 {
 	data->mlx->mlx = mlx_init();
@@ -39,4 +51,6 @@ void	init_mlx(t_data *data)
 			WINDOW_WIDTH, WINDOW_HEIGHT, "Cub3D");
 	if (!data->mlx->win)
 		print_error_and_exit("mlx_new_window failed\n");
+	init_texture(data);
+	init_img(data);
 }
