cd5b3d0cff10cc194437f4582c605a4e59b82fa8
Ishi <syi378039@gmail.com>
2024-03-20 04:29:18 +0900

    dda追加あ

--- includes/draw.h
+++ includes/draw.h
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 16:11:24 by tsishika          #+#    #+#             */
-/*   Updated: 2024/03/20 02:35:15 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/03/20 04:28:47 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,6 @@ void			draw_minimap(t_data *data);
 void			my_mlx_pixel_put(t_data *data, int x, int y, int color);
 
 // test_draw.c
-void			init_ray(t_data *data, t_ray *ray, int x);
-void			calculate_step_and_side_dist(t_data *data, t_ray *ray);
-void			perform_dda(t_data *data, t_ray *ray);
-void			calculate_wall_height(t_data *data, t_ray *ray, t_wall *wall);
 void			draw_wall(t_data *data, t_wall *wall, int x);
 
 // xpm_file_to_img.c
--- srcs/draw/test_draw.c
+++ srcs/draw/test_draw.c
@@ -6,39 +6,12 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 01:29:21 by tsishika          #+#    #+#             */
-/*   Updated: 2024/03/20 01:43:30 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/03/20 04:28:11 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "draw.h"
 #include <math.h>
-
-void	perform_dda(t_data *data, t_ray *ray)
-{
-	while (ray->hit == 0)
-	{
-		if (ray->side_dist_x < ray->side_dist_y)
-		{
-			ray->side_dist_x += ray->delta_dist_x;
-			ray->map_x += ray->step_x;
-			if (ray->step_x == 1)
-				ray->side = 0;
-			else
-				ray->side = 1;
-		}
-		else
-		{
-			ray->side_dist_y += ray->delta_dist_y;
-			ray->map_y += ray->step_y;
-			if (ray->step_y == 1)
-				ray->side = 2;
-			else
-				ray->side = 3;
-		}
-		if (data->cub->map->map_data[ray->map_y][ray->map_x] == '1')
-			ray->hit = 1;
-	}
-}
 
 void	draw_wall(t_data *data, t_wall *wall, int x)
 {
